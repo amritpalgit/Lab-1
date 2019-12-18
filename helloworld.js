02bd6718b677399a8260be850a2797017e150124
amritpalgit <58850067+amritpalgit@users.noreply.github.com>
2019-12-18 22:38:19 +0530

    passing comments use edit file on github

--- helloworld.js
+++ helloworld.js
@@ -2,5 +2,5 @@ var phellotext="";
 phellotext=phellotext+"<html><head><title> </title></head> <body>";
 phellotext=phellotext+"<div style='width:90%;margin-left:5%;margin-top:30%;float:left;'><h1> Hello World </h1> </div>";
 phellotext=phellotext+"</body> </html>";
-document.write("Hello World!");
-console.log("Hello, World!");
+document.write("Hello World");// for front end user
+console.log("Hello, World");// for consol becuse of developer check controls
