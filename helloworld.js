3591e9ce42c532e434b43ea4f8da73f1c470d06f
amritpalgit <58850067+amritpalgit@users.noreply.github.com>
2019-12-18 22:37:02 +0530

    uploaded file

---
+++ helloworld.js
@@ -0,0 +1,6 @@
+var phellotext="";
+phellotext=phellotext+"<html><head><title> </title></head> <body>";
+phellotext=phellotext+"<div style='width:90%;margin-left:5%;margin-top:30%;float:left;'><h1> Hello World </h1> </div>";
+phellotext=phellotext+"</body> </html>";
+document.write("Hello World!");
+console.log("Hello, World!");
